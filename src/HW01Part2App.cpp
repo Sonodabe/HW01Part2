68e16116a8a0f46fc34d208d5ff1423a9d5a7b80
Brandon Sonoda <sonodabe@muohio.edu>
2012-09-06 23:26:17 -0400

    Added review

--- src/HW01Part2App.cpp
+++ src/HW01Part2App.cpp
@@ -41,6 +41,7 @@ class HW01Part2App : public AppBasic {
 	void tint(uint8_t* pixels, Color8u color);
 	void blur(uint8_t* pixels);
 	void drawLine(uint8_t* pixels, int startX, int startY, int endX, int endY, Color8u color);
+    void drawPixel(uint8_t* pixels, int posX, int posY, Color8u color);
 	
 	private:
 	Surface* mySurface_;
@@ -76,6 +77,15 @@ void HW01Part2App::setup()
 	aniY = 0;
 }
 
+void HW01Part2App::drawPixel(uint8_t* pixels, int posX, int posY, Color8u color){
+    int offset = 3*(posX + posY*kTextureSize);
+    pixels[offset] = color.r;
+    pixels[offset+1] = color.g;
+    pixels[offset+2] = color.b;
+    
+}
+
+
 /*
 * This is a non-working attempt at goal A.4.  It colors the whole screen color1 rather than creating a gradient.
 */
@@ -107,12 +117,21 @@ void HW01Part2App::tint(uint8_t* pixels, Color8u color)
 		int ratio = y/kAppHeight;
 		for(int x = 0; x <= kAppWidth; x++)
 		{
-            //Brandon Sonoda: I think that the variable "offset" doesn't well represent
-            //the value.. maybe "index" would be better?
+            /*Brandon Sonoda: I think that the variable "offset" doesn't well represent
+            the value.. maybe "index" would be better?
+            and since you're dividing integers by two you're actually going to truncate data
+            Twice... Two fixes you can do for this is: 
+             
+             1.less preferred: add them, then divide by two making only one truncation
+              = (pixels[index]+color.r)/2;
+             2.Converting them into a double, dividing by two, then rounding that number
+             = round((double)(pixels[offset]+color.r)/2;
+             */
 			int offset = 3*(x + y*kTextureSize);
 			pixels[offset] = pixels[offset]/2 + color.r/2;
 			pixels[offset+1] = pixels[offset+1]/2 + color.g/2;
 			pixels[offset+2] = pixels[offset+2]/2 + color.b/2;
+             
 		}
 	}
 }
@@ -124,19 +143,31 @@ void HW01Part2App::tint(uint8_t* pixels, Color8u color)
 */
 void HW01Part2App::drawCircle(uint8_t* pixels, int center_x, int center_y, int radius, Color8u color)
 {
+    int dist;
 	for(int y = center_y - radius; y <= center_y + radius; y++)
 	{
 		for(int x = center_x - radius; x <= center_x + radius; x++)
 		{
 			//test for accessing array out of bounds
+            /*Brandon Sonoda:
+             It might be better to declare variables outside of loops, 
+             that way a new one doesnt have to be declared everytime through the loop
+             but instead, it is just changed everytime
+             */
+            
+            /* Brandon Sonoda:
+             But instead of having this little snippet in all of your draw methods, you could
+             take it out, and make this its own method, making the code more readable.
+             */
 			if(y < 0 || x < 0 || x >= kAppWidth || y >= kAppHeight) continue;
-			int dist = (int)sqrt((double)((x-center_x)*(x-center_x) + (y-center_y)*(y-center_y)));
+			/*int*/ dist = (int)sqrt((double)((x-center_x)*(x-center_x) + (y-center_y)*(y-center_y)));
 			if(dist <= radius)
 			{
-					int offset = 3*(x + y*kTextureSize);
+					/*int offset = 3*(x + y*kTextureSize);
 					pixels[offset] = color.r;
 					pixels[offset+1] = color.g;
-					pixels[offset+2] = color.b;
+					pixels[offset+2] = color.b;*/
+                drawPixel(pixels, x, y, color);
 			}
 		}
 	}
@@ -155,11 +186,11 @@ void HW01Part2App::drawRect(uint8_t* pixels, int corner_x, int corner_y, int wid
 		{
 			//test for accessing array out of bounds
 			if(y < 0 || x < 0 || x >= kAppWidth || y >= kAppHeight) continue;
-					int offset = 3*(x + y*kTextureSize);
 					//By blending the colors I get a semi-transparent effect
-					pixels[offset] = color.r;
-					pixels[offset+1] = color.g;
-					pixels[offset+2] = color.b;
+            /*Brandon Sonoda: Converted into method here:*/
+            drawPixel(pixels, x, y, color);
+
+					
 		}
 	}
 }
@@ -179,7 +210,7 @@ void HW01Part2App::blur(uint8_t* pixels)
 	{
 		for(int x = 1; x <= kAppWidth-1; x++)
 		{
-			int offset = 3*(x + y*kTextureSize);
+			//int offset = 3*(x + y*kTextureSize);
 			uint8_t r_total = 0;
 			uint8_t g_total = 0;
 			uint8_t b_total = 0;
@@ -193,9 +224,12 @@ void HW01Part2App::blur(uint8_t* pixels)
 					b_total += pixels[offset2 + 2];
 				}
 			}
-			pixels[offset] = r_total/9;
+            /* Brandon Sonoda: Again, it doesn't really matter with this project, 
+             but be mindful of truncation.. I'm passionate about really weird issues */
+			/*pixels[offset] = r_total/9;
 			pixels[offset + 1] = g_total/9;
-			pixels[offset + 2] = b_total/9;
+			pixels[offset + 2] = b_total/9;*/
+            drawPixel(pixels, x, y, Color8u(r_total/9, g_total/9, b_total/9));
 			
 		}
 	}
@@ -212,18 +246,25 @@ void HW01Part2App::drawLine(uint8_t* pixels, int startX, int startY, int endX, i
 {
 	int xDist = abs(startX - endX);
 	int yDist = abs(startY - endY);
+    
+    /*Brandon Sonoda:
+     For my draw line method I actually just found the angle of elevation of the line using the tangent function, 
+     and used the atan() function to determine the y value for any given x... this cut down on the amount of hard
+     coding that you would have to do, and this way you would only have to check if the line were vertical, reducing
+     the amount of conditionals (I couldnt find the angle of elevation for a vertical line w/o diving by zero)
+     
+     ... But the implementation of this line drawer works perfectly finely
+     */
+    
+
 	int x = startX;
 	int y = startY;
+
 	//handle 45 degree lines
 	if(xDist == yDist)
 	{
 		for(y = min(startY, endY); y <= max(startY, endY); y++)
-		{
-			int offset = 3*(x + y*kTextureSize);
-			pixels[offset] = color.r;
-			pixels[offset + 1] = color.g;
-			pixels[offset + 2] = color.b;
-		}
+            drawPixel(pixels, x, y, color);
 	}
 	//bresenham's line algorithm
 	else if(xDist > yDist)
@@ -231,21 +272,20 @@ void HW01Part2App::drawLine(uint8_t* pixels, int startX, int startY, int endX, i
 		for(x = min(startX, endX); x <= max(startX, endX); x++)
 		{
 			y = (endY - startY)/(endX - startX)*(x - startX) + startY;
-			int offset = 3*(x + y*kTextureSize);
-			pixels[offset] = color.r;
-			pixels[offset + 1] = color.g;
-			pixels[offset + 2] = color.b;
+            drawPixel(pixels, x, y, color);
+
 		}
 	}
-	else if(xDist < yDist)
+    /*Brandon Sonoda: You dont really need to check if xDist is less than yDist here, because you know it's not equal and you
+     know it's not greater than, so it must be less than.
+    */
+	else /*if(xDist < yDist)*/
 	{
 		for(y = min(startY, endY); y <= max(startY, endY); y++)
 		{
 			x = (y - startY)/(endY - startY)*(endX - startX) + startX;
-			int offset = 3*(x + y*kTextureSize);
-			pixels[offset] = color.r;
-			pixels[offset + 1] = color.g;
-			pixels[offset + 2] = color.b;
+            drawPixel(pixels, x, y, color);
+
 		}
 	}
 }
@@ -262,15 +302,23 @@ void HW01Part2App::update()
 	drawRect(dataArray, 400, 400, 50, 100, Color8u(255,0,0));
 	drawCircle(dataArray, 450, 200, 100, Color8u(0,0,255));
 	//animation, circle moves across the screen, leaving a trail of color behind it
-	aniX++;
+	//aniX++;
 	aniY++;
-	if(aniX >= kAppWidth)
-		aniX = 0;
+	//if(aniX >= kAppWidth)
+		//aniX = 0;
+    
+    /*Brandon Sonoda: You don't really need a ternary operator here, but it's a shorter (visually) alternative,
+     and if you intended for the circle to disappear off screen before you relocated it, just make sure to account
+     for the circles radius*/
+    aniX = (aniX>=kAppWidth)? 0:aniX+1;
 	if(aniY >= kAppHeight)
 		aniY = 0;
 	drawCircle(dataArray, aniX, aniY, 100, Color8u(100,255,100));
 
 	drawLine(dataArray, 10, 10, 50, 100, Color8u(255, 255, 255));
+    /*Brandon Sonoda:  This seems like a waste, asking the if it's been saved every time, but it seems like 
+     this is the best place to put it... unless you were to put it into the mouse down method, but that wouldn't
+     insure that the picture is captured.... */
 	//write the image to a .png if we haven't already done so
 	if(!hasSaved)
 	{
@@ -282,12 +330,14 @@ void HW01Part2App::update()
 	*
 	* Satisfies goal E.6.
 	*/
+    
 	if(hasClicked)
 	{
 		tint(dataArray, Color8u(255,255,255));
 		blur(dataArray);
 		hasClicked = false;
 	}
+     
 }
 
 void HW01Part2App::draw()
